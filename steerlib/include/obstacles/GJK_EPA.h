7a549c8d8fa58f4bdee3b05d9fb3c4ac7c93248b
Jeff Ames <jeff.ames@cs.rutgers.edu>
2015-10-24 08:31:41 -0400

    Minor cleanup

--- steerlib/include/obstacles/GJK_EPA.h
+++ steerlib/include/obstacles/GJK_EPA.h
@@ -1,69 +1,25 @@
-/*!
-*
-* \author VaHiD AzIzI
-*
-*/
-
-
 #ifndef GJK_EPA_H_
 #define GJK_EPA_H_
 
+#include <vector>
 
 #include "util/Geometry.h"
 
-
-#include <vector>
-
-
 namespace SteerLib
 {
-
     class STEERLIB_API GJK_EPA
     {
         public:
 			GJK_EPA();
-
-            /*
-             *  Testing:
-             *
-             *  To test your code, execute the following command. The
-             *  output you see should be:
-             *
-             *  $ ./steersim -testcase polygons_test -ai collisionAI
-             *
-             *  loaded module collisionAI
-             *  loaded module testCasePlayer
-             *  Initializing...
-             *  Preprocessing...
-             *   Collision detected between polygon No.0 and No.1 with a penetration depth of 2 and penetration vector of (-0,0,-1)
-             *  Simulation is running...
-             *
-             *  ----------------------------------------------------
-             *
-             *  Additional Notes:
-             *
-             *  intersection will be called for all polygons, for all
-             *  unique pairs, ie. if we have 3 polygons it is called 3
-             *  times:
-             *
-             *  one between polygon No. 1 and No. 2
-             *  one between polygon No. 1 and No. 3
-             *  one between polygon No. 2 and No. 3
-             *
-             *  For grading your results will be compared by our
-             *  results on testcase polygons1
-             */
-            static bool intersect(float& return_penetration_depth, Util::Vector& return_penetration_vector, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB);
+            static bool intersect(float& penetration_depth, Util::Vector& penetration_vector, const std::vector<Util::Vector>& shapeA, const std::vector<Util::Vector>& shapeB);
 
         private:
             static Util::Vector support(const std::vector<Util::Vector>& shape, const Util::Vector& direction);
             static bool simplexContainsOrigin(std::vector<Util::Vector>& simplex, Util::Vector& direction);
             static bool GJK(std::vector<Util::Vector>& simplex, const std::vector<Util::Vector>& A, const std::vector<Util::Vector>& B);
             static void EPA(float& penetration_depth, Util::Vector& penetration_vector,  const std::vector<Util::Vector>& A, const std::vector<Util::Vector>& B,  std::vector<Util::Vector>& simplex);
-            static void Find_Closest_edge(std::vector<Util::Vector>& simplex, Util::Vector& direction);
-    }; // class GJK_EPA
+            static void findClosestEdge(std::vector<Util::Vector>& simplex, Util::Vector& direction);
+    };
+}
 
-} // namespace SteerLib
-
-
-#endif /* GJK_EPA_H_ */
+#endif
--- steerlib/src/GJK_EPA.cpp
+++ steerlib/src/GJK_EPA.cpp
@@ -1,12 +1,4 @@
-/*!
-*
-* \author VaHiD AzIzI
-*
-*/
-
-
 #include "obstacles/GJK_EPA.h"
-
 
 SteerLib::GJK_EPA::GJK_EPA()
 {
@@ -14,9 +6,11 @@ SteerLib::GJK_EPA::GJK_EPA()
 
 Util::Vector SteerLib::GJK_EPA::support(const std::vector<Util::Vector>& shape, const Util::Vector& direction)
 {
-    double max = 0.0;
+    assert(shape.size() > 0);
+
+    double max = dot(*shape.begin(), direction);
     Util::Vector point;
-    max = dot(*shape.begin(),direction);
+
     for (std::vector<Util::Vector>::const_iterator iter = shape.begin(); iter != shape.end(); ++iter) {
         double thisDot = dot(*iter, direction);
         if (thisDot > max) {
@@ -25,23 +19,6 @@ Util::Vector SteerLib::GJK_EPA::support(const std::vector<Util::Vector>& shape, 
         }
     }
     return point;
-    
-    /*
-    Util::Vector point;
-    int max_index=0;
-    
-    for (int i=0; i<sizeof(shape);++i)
-    {
-
-        if(dot(shape[max_index],direction)<dot(shape[i],direction))
-            max_index=i;
-    }
-   point = shape[max_index];
-    std::cerr<<" max index ."<<max_index<<"value"<<dot(shape[max_index],direction)<<std::endl;
-    std::cerr<<" sizeof(shape)" <<sizeof(shape)<<std::endl;
-
-return point;*/
- 
 }
 
 bool SteerLib::GJK_EPA::simplexContainsOrigin(std::vector<Util::Vector>& simplex, Util::Vector& direction)
@@ -62,12 +39,12 @@ bool SteerLib::GJK_EPA::simplexContainsOrigin(std::vector<Util::Vector>& simplex
     if (dot(normalToCA, CO) > 0) {
         // origin is beyond CA
         simplex.erase(simplex.begin() + 1); // delete B
-        direction = normalToCA; //new direction.
+        direction = normalToCA; // new direction.
         return false;
     } else if (dot(normalToCB, CO) > 0) {
         // origin is beyond CB
         simplex.erase(simplex.begin()); // delete A
-        direction = normalToCB; //new direction.
+        direction = normalToCB; // new direction.
         return false;
     } else {
         // simplex contains origin
@@ -77,137 +54,116 @@ bool SteerLib::GJK_EPA::simplexContainsOrigin(std::vector<Util::Vector>& simplex
 
 bool SteerLib::GJK_EPA::GJK(std::vector<Util::Vector>& simplex, const std::vector<Util::Vector>& shapeA, const std::vector<Util::Vector>& shapeB)
 {
+    // initial directions
     Util::Vector D0(1, 0, 0);
     Util::Vector D1(0, 0, 1);
-    Util::Vector tempA = support(shapeA, D0);
-    Util::Vector tempB= support(shapeB,D0);
+
     Util::Vector A = support(shapeA, D0) - support(shapeB, -D0);
-    std::cerr<<"tempA"<<tempA<<"tempB"<<tempB<<std::endl;
     Util::Vector B = support(shapeA, D1) - support(shapeB, -D1);
-    std::cerr<<"A"<<A <<"B"<<B<<std::endl;
-   // std::cerr<<" shapeA "<<shapeA<<"shapeB"<<shapeB<<std::endl;
-  
-  std::cerr<<"A"<<A <<"B"<<B<<std::endl;
+
+    std::cerr << "supp(shA, D0) = " << support(shapeA, D0) << ", supp(shB, D0) = " << support(shapeB, D0) << std::endl;
+    std::cerr << "A = " << A  << ", B = " << B << std::endl;
+    // std::cerr << "shapeA = " << shapeA << ", shapeB = " << shapeB << std::endl;
+
     Util::Vector AB = B - A;
     Util::Vector AO = -A;
     simplex.clear();
     simplex.push_back(A);
     simplex.push_back(B);
-    Util::Vector D = cross(cross(AB, AO), AB); //direction to 0
+    Util::Vector D = cross(cross(AB, AO), AB); // direction to 0
     while (true) {
-        Util::Vector C = support(shapeA, D) - support(shapeB, -D); //new C vertex.
-        
-        
-        std::cerr<<"c"<<C<<"D"<<D<<std::endl;
-        
+        Util::Vector C = support(shapeA, D) - support(shapeB, -D); // new C vertex.
+
+        std::cerr << "C = " << C << ", D = " << D << std::endl;
+
         if (dot(C, D) < 0)
-            return false; //no collision.s
+            return false; // no collision
         simplex.push_back(C);
-        bool contains_origin = simplexContainsOrigin(simplex, D);
-        if (contains_origin)
+        if (simplexContainsOrigin(simplex, D))
             return true;
     }
 }
 
-
 void SteerLib::GJK_EPA::EPA(float& penetration_depth, Util::Vector& penetration_vector,  const std::vector<Util::Vector>& A,  const std::vector<Util::Vector>& B,  std::vector<Util::Vector>& simplex)
 {
     // Calculate penetration
-    // penetration_depth = ...
-    // penetration_vector = ...
     Util::Vector E0(0,0,0);
-    
+
     while(true)
     {
-        
-        SteerLib::GJK_EPA::Find_Closest_edge(simplex,E0); //return E0 : normal vector.
-        Util::Vector new_Vertex = support(A,E0)-support(B,-E0); //new point.
-        //dot(O new_vertex,projection E0) ==magnitude of EO?)
-        
-        
-        
-        if (abs(E0.norm()-dot(new_Vertex,E0/E0.norm()))<0.00001) // until find minkowski
-        {
-            //std::cerr<<abs(E0.norm()-dot(new_Vertex,E0/E0.norm()));
-            //" Collision detected between polygon No."<<i<<" and No."<<j<< " with a penetration depth of "<< penetration_depth << " and penetration vector of "<<penetration_vector<<std::endl;
+        SteerLib::GJK_EPA::findClosestEdge(simplex, E0); // return E0 : normal vector.
+        Util::Vector new_Vertex = support(A, E0) - support(B, -E0); // new point.
+        // dot(O new_vertex, projection E0) ==magnitude of EO?)
+
+        // until find minkowski
+        if (abs(E0.norm() - dot(new_Vertex, E0/E0.norm())) < 0.00001) {
+            // std::cerr << abs(E0.norm()-dot(new_Vertex,E0/E0.norm()));
+            // " Collision detected between polygon No." << i << " and No." << j <<  " with a penetration depth of " << penetration_depth << " and penetration vector of " << penetration_vector << std::endl;
             penetration_depth = E0.norm();
             penetration_vector = E0/E0.norm();
             break;
-        }
-        
-        else
-            //add simplex.
+        } else {
+            // add simplex.
             simplex.push_back(new_Vertex); // and loop.
-        
+        }
     }
-    
 }
 
-void SteerLib::GJK_EPA::Find_Closest_edge(std::vector<Util::Vector>& simplex, Util::Vector& direction)
+void SteerLib::GJK_EPA::findClosestEdge(std::vector<Util::Vector>& simplex, Util::Vector& direction)
 {
     Util::Vector A = simplex[0];
     Util::Vector B = simplex[1];
     Util::Vector C = simplex[2];
-    
+
     std::vector<Util::Vector> edge;
-    edge.push_back(B-A); //AB
-    edge.push_back(C-B); //BC
-    edge.push_back(A-C); //CA
-    
+    edge.push_back(B - A); // AB
+    edge.push_back(C - B); // BC
+    edge.push_back(A - C); // CA
+
     std::vector<float> min_value;
     std::vector<Util::Vector> normal_to_edge;
-    
-   // std::vector<Util::Vector> normal_to edge;
+
+    // std::vector<Util::Vector> normal_to edge;
     min_value.push_back(10);
-    for (int i=0;i<sizeof(normal_to_edge);++i)
+    for (int i=0; i < sizeof(normal_to_edge); ++i)
     {
-        normal_to_edge.push_back(cross(cross(edge[i],-simplex[i]),edge[i])); //direction.
-        min_value.push_back(dot(simplex[i],normal_to_edge[i]/normal_to_edge[i].norm())); //value
-        
+        normal_to_edge.push_back(cross(cross(edge[i], -simplex[i]), edge[i])); // direction.
+        min_value.push_back(dot(simplex[i], normal_to_edge[i]/normal_to_edge[i].norm())); // value
     }
-    
-    //find min value;
-    
-    int min_index=0;
-    
-    for (int i=0;i<sizeof(min_value);++i)
-    {
-        if(min_value[min_index]>min_value[i])
-            min_index=i;
-        
-    }
+
+    // find min value;
+    int min_index = 0;
+    for (int i = 0; i < sizeof(min_value); ++i)
+        if (min_value[min_index] > min_value[i])
+            min_index = i;
+
     direction = normal_to_edge[min_index];
 }
 
 /**
- * @brief Performs the GJK and EPA algorithms to detect
- * collisions. Note this assumes all shapes are 2D and for all points
- * (x,y,z), y=0.
+ * @brief Detect collisions using the GJK and EPA algorithms. Note
+ * this assumes all shapes are 2D and y = 0 for all points.
  *
- * @field return_penetration_depth If there is a collision, this
- * variable is set to the penetration depth calculated by the EPA
- * algorithm
+ * @field penetration_depth  If there is a collision, this variable is
+ * set to the penetration depth calculated by the EPA
+ * algorithm. Otherwise it's set to zero.
  *
- * @field return_penetration_vector If there is a collision, this
- * variable is set to the penetration vector calculated by the EPA
- * algorithm
+ * @field penetration_vector  If there is a collision, this variable is
+ * set to the penetration vector calculated by the EPA
+ * algorithm. Otherwise it's set to the zero vector.
  *
- * @field _shapeA  The first input shape
- * @field _shapeB  The second input shape
- *
- * ----------------------------------------------------
- * Algorithms:
- * Implement the GJK and EPA Algorithms in 2D since for all points (x,0,z), y=0.
+ * @field shapeA  The first input shape
+ * @field shapeB  The second input shape
  */
-bool SteerLib::GJK_EPA::intersect(float& return_penetration_depth, Util::Vector& return_penetration_vector, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB)
+bool SteerLib::GJK_EPA::intersect(float& penetration_depth, Util::Vector& penetration_vector, const std::vector<Util::Vector>& shapeA, const std::vector<Util::Vector>& shapeB)
 {
     std::vector<Util::Vector> simplex;
-    if (GJK(simplex, _shapeA, _shapeB)) {
-        EPA(return_penetration_depth, return_penetration_vector, _shapeA, _shapeB, simplex);
+    if (GJK(simplex, shapeA, shapeB)) {
+        EPA(penetration_depth, penetration_vector, shapeA, shapeB, simplex);
         return true;
     } else {
-        return_penetration_depth = 0;
-        return_penetration_vector.zero();
+        penetration_depth = 0;
+        penetration_vector.zero();
         return false;
     }
 }
